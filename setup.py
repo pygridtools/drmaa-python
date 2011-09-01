589b46aaf989b71b9041bf4504aaf0ba11a2544c
Enrico Sirola <enrico.sirola@statpro.com>
2011-09-01 11:13:34 +0200

    version 0.4

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ package_data = dict([ (x, ['test/*.py']) for x in packages])
 
 setup(
     name="drmaa",
-    version="0.4b3",
+    version="0.4",
     packages=packages,
     package_data=package_data,
     author="Enrico Sirola",
