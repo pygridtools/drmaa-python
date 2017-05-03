6e10c55869262c5e3f31102d2015b2d6ed9f78c5
Will Furnass <will@thearete.co.uk>
2017-05-03 15:42:10 +0100

    Replace Google Code URL with GitHub/PyPI URLs (#51)
    * Replace Google Code URL with GitHub URLs
    
    * Note that releases available from PyPi

--- drmaa/__init__.py
+++ drmaa/__init__.py
@@ -21,8 +21,10 @@
 A python package for DRM job submission and control.
 
 This package is an implementation of the DRMAA 1.0 Python language
-binding specification (http://www.ogf.org/documents/GFD.143.pdf).  See
-http://drmaa-python.googlecode.com for package info and download.
+binding specification (http://www.ogf.org/documents/GFD.143.pdf).
+The source is hosted on GitHub: https://github.com/pygridtools/drmaa-python
+Releases are available from PyPi: https://pypi.python.org/pypi/drmaa
+Documentation is hosted on ReadTheDocs: http://drmaa-python.readthedocs.org/
 
 :author: Enrico Sirola (enrico.sirola@statpro.com)
 :author: Dan Blanchard (dblanchard@ets.org)
